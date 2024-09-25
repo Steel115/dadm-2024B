156c93fa8e4d3b0b901c300da1f63cab5708e9ee
Enrique <150816038+Steel115@users.noreply.github.com>
2024-09-25 18:42:03 +0000

    ⭐ FEAT: Radio button

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ const items = ref([
     {id:'3', label: '10 chetos'}
 ]);
 const newItem = ref(''); 
+const newItemPriority = ref('low');
 </script>
 
 <template>
@@ -16,7 +17,11 @@ const newItem = ref('');
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
     </h1>
-    <input v-model="newItem" type="text" placeholder="Agregar artiuclo"> {{newItem}}
+    <input v-model="newItem" type="text" placeholder="Agregar artiuclo">
+    <!-- Radio Buttons -->
+     <label><input type="radio" value="low" v-model="newItemPriority">Bajo</label>
+     <label><input type="radio" value="high" v-model="newItemPriority">Alta</label>
+     {{ newItemPriority == 'low' ? '🧊' : '🔥'}}
     <ul>
         <li
          v-for="({id,label}, i) in items" :key="id"> {{ i+1 }} {{i%2==0?'🔥':'🛍️'}} {{label}} </li>

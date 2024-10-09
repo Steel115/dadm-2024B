eecf1d2b4f57ab054c721486eaf7462a073335ac
Steel <kikin.herrera28@gmail.com>
2024-10-09 12:13:07 -0600

    ⭐FEAT: sesion 13

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@ import { ref } from "vue";
 const header = ref('App lista de Compras');
 // ---Items---
 const items = ref([
-    {id:'0', label: '10 bolillos'}, 
-    {id:'1', label: '1 lata de volt'}, 
-    {id:'2', label: '1 bote de café'},
-    {id:'3', label: '10 chetos'}
+    {id:'0', label: '10 bolillos',purchased: true, priority: true}, 
+    {id:'1', label: '1 lata de volt',purchased: true, priority: true}, 
+    {id:'2', label: '1 bote de café',purchased: false, priority: false},
+    {id:'3', label: '10 chetos',purchased: false, priority: false}
 ]);
 //Item-Method
 const saveItem = () => {
@@ -20,30 +20,36 @@ const saveItem = () => {
 
 const newItem = ref(''); 
 const newItemHighPriority = ref(false);
-
+const editing = ref(false);
+const activateEdition = (activate) =>{ 
+    editing.value = activate;
+};
 //metodo para crear el hipervinculo
-const hipervinculo = () => {
+/*const hipervinculo = () => {
     return newItem.value === '' ? 'https://www.google.com' :
     'https://' + newItem.value;
-}
+}*/
 
 </script>
 
-
 <template>
+<div class="header">
 <h1>
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
-</h1>
+  </h1>
+  <button v-if="editing" class="btn" @click="activateEdition(false)">CANCELAR</button>
+  <button v-else class="btn btn-primary" @click="activateEdition(true)">AGREGAR ARTICULO</button>
+</div>
 
 <!-- Hipervinculo -->
-<a v-bind:href="hipervinculo()" target="_blank">
+<!--<a v-bind:href="hipervinculo()" target="_blank">
   {{ newItem == '' ? 'link' : newItem }}
-</a>
+</a>-->
 
 <!-- Agrupando en un div las entradas -->
-<form class="add-item form" v-on:submit.prevent="saveItem">
-    
+<form v-on:submit.prevent="saveItem()" v-if="editing" class="add-item fomr">
+
     <!-- entrada de texto -->
     <input
       v-model.trim="newItem"
@@ -57,13 +63,33 @@ const hipervinculo = () => {
     </label>
 
     <!-- Boton -->
-    <button class="btn btn-primary"> Guardar </button>
+    <button class="btn btn-primary" 
+    :disabled="newItem.length === 0"
+    > Guardar </button>
   
 </form>
-    <!-- Lista de items -->
+    <!-- Lista de items objetos-->
     <ul>
-        <li v-for="({id,label}, i) in items" :key="id"> {{ i+1 }} {{i%2==0?'🔥':'🛍️'}} {{label}} </li>
-    </ul>
+    <li
+         v-for="{label, id, purchased, priority} in items" 
+         :key="id"
+         class="amazing" 
+         :class="{strikeout: purchased, priority: priority}"> 
+         {{priority ? "🔥": "🛍️"}}
+        {{label}} </li>
+  </ul>
+   
+  <!-- Lista de items con arreglos-->
+  <ul>
+    <li
+         v-for="{label, id, purchased, priority} in items" 
+         :key="id"
+         
+         :class="[purchased ? 'strikeout' : '', priority ? 'priority' : '']"> 
+         {{priority ? "🔥": "🛍️"}}
+        {{label}} </li>
+  </ul>
+  <p v-if="items.length === 0"> 🥀NO HAY ELEMENTOS EN TU LISTA 🥀</p>
 </template>
 
 <style scoped>

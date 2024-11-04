2cfe30d77e1a675dfdaec6829d5e454c0c83081b
Enrique <150816038+Steel115@users.noreply.github.com>
2024-11-04 23:59:31 +0000

    ⭐FEAT: Starting Project

---
+++ src/components/plan-item.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="plan">
+
+      <div class="description">
+        
+        <span class="title">
+          El adicto
+        </span>
+      
+      </div>
+    
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>

eb11d5008569711b2103a1d4a67f04de70919cb7
Steel115 <kikin.herrera28@gmail.com>
2024-11-08 11:47:58 -0600

    ✨S03S04

---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,15 @@
+<script setup>
+import PlanPickerItem from './PlanPickerItem.vue';
+import {ref} from 'vue';
+const plans= ref(['El cafetero', 'El chelero', 'El intelectual']);
+</script>
+
+<template>
+    <div class="plans">
+      <PlanPickerItem v-for="plan in plans" :key="plan" :name="plan" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>

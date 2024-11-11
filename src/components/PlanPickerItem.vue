4691e868678d68c26d08fb7d277705afb1404b58
Steel115 <kikin.herrera28@gmail.com>
2024-11-11 12:50:53 -0600

    ⭐ S03S05 Events Add events to child component

--- src/components/PlanPickerItem.vue
+++ src/components/PlanPickerItem.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="plan">
+  <div @:click="selectPlan" class="plan">
 
     <div class="description">
       
-      <span class="title">
-       {{ name }}
-      </span>
+      <span class="title">{{ name }} {{ selected ? '✓' : '' }}</span>
     
     </div>
   
@@ -13,15 +11,29 @@
 </template>
 
 <script setup>
+//impor ref functio
+import {ref} from 'vue';
 //Usando un macro para
 //defenir las props
 //const props = defineProps(['name','planIcon']);
-defineProps({
+const props = defineProps({
     name: {
       type: String,
       required: true
     }
   });
+  //Definir un evento (macro)
+  const emit = defineEmits(['select']);
+  //variable reactiva que permite recordar 
+  // si el plan ha sido seleccionado
+  const selected = ref(false);
+  //funcion que permite cambiar el estado de la variable
+  const selectPlan = () => {
+    selected.value =true;
+    //emitir un evento de que el
+    //plan ha sido seleccionado
+    emit('select', props.name);
+  }
 </script>
 
 <style scoped>

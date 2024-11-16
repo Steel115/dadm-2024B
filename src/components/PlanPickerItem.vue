acb003d3daafe149b8cbacdc0efaaf5abc6d6023
Steel115 <kikin.herrera28@gmail.com>
2024-11-16 14:25:44 -0600

    Error solucionado

--- src/components/PlanPickerItem.vue
+++ src/components/PlanPickerItem.vue
@@ -2,11 +2,9 @@
   <div @:click="selectPlan" class="plan">
 
     <div class="description">
-      
       <span class="title">{{ name }} {{ isSelected ? '✓' : '' }}</span>
-    
     </div>
-  
+
   </div>
 </template>
 
@@ -14,32 +12,29 @@
 //importamos la funcion computed de vue para la propiedad computada
 import { computed } from 'vue';
 
-//borramos ref
 //defenir las props
-//const props = defineProps(['name','planIcon']);
 const props = defineProps({
-    name: {
-      type: String,
-      required: true
-    },
-    selectPlan: String
-  });
+  name: {
+    type: String,
+    required: true
+  },
+  selectPlan: String
+});
 
-  //Definir un evento (macro)
+//Definir un evento (macro)
 const emit = defineEmits(['select']);
- //funcion que permite cambiar el estado de la variable
-  const selectPlan = () => {
-    //Emitir un evento de que el
-    //plan ha sido seleccionado
-    emit('select', props.name);
-  };
-  //creando una propiedad computada 
-  //para saber si el plan ha sido seleccionado
-  const isSelected = computed(() => {
-    return props.name === props.selectedPlan;
-  });
+//funcion que permite cambiar el estado de la variable
+const selectPlan = () => {
+  //Emitir un evento de que el plan ha sido seleccionado 
+  emit('select', props.name);
+};
+
+/*creando una propiedad computada. Para saber si el 
+plan ha sido seleccionado */
+const isSelected = computed(() => {
+  return props.name === props.selectPlan;
+});
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

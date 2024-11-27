b59e92523355f25acc973168c170ca53010a64ae
Steel115 <kikin.herrera28@gmail.com>
2024-11-27 11:51:53 -0600

    ⭐S03S06 Hooks

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,6 +1,6 @@
 <script setup>
 import PlanPickerItem from './PlanPickerItem.vue';
-import {ref, onMounted} from 'vue';
+import {ref, onMounted, onUnmounted} from 'vue';
 /*Creando una variable reactiva para 
 almacenar el plan seleccionado*/
 const selectedPlan = ref(null);
@@ -12,17 +12,23 @@ const selectPLan = (name) => {
       };
 
 const plansWrapper = ref(null);
+//intentando obtener la referencia al elemento .plans
+console.log("termina de ejecutar el scrip setup");
 
  //intentando obtener la referencia al elemento .plans
  onMounted(() => {
   console.log("El componente PlanPicker fue montado 😎");
   console.log(plansWrapper.value);
  })
+ //registrando el hook mounted
+onUnmounted(()=>{
+  console.log("El componente PlanPicker se ha desmontado");
+ })
 </script>
  
 <template>
     <div ref="plansWrapper" class="plans">
-      <plansWrapper
+      <PlanPickerItem
       v-for="plan in plans" 
       :key="plan" 
       :name="plan" 

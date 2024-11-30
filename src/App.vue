cd38dae79788080c1a1bde169e5249e4e0294db8
Enrique <150816038+Steel115@users.noreply.github.com>
2024-11-30 23:05:47 +0000

    ⭐S04A02 Slots

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,16 @@
 <script setup>
-import { ref } from "vue";
-//modelo 
-const header = ref('App lista de Compras');
-const items = ref([
-    {id:'0', label: '10 bolillos'}, 
-    {id:'1', label: '1 lata de volt'}, 
-    {id:'2', label: '1 bote de café'},
-    {id:'3', label: '10 chetos'}
-]);
-const newItem = ref(''); 
-const newItemHighPriority = ref(false);
+import fancyButton from './components/FancyButton.vue';
 </script>
 
 <template>
-<h1>
-    <i class="material-icons shopping-cart-icon">local_mall</i>
-    {{ header }}
-</h1>
-
-<!-- Agrupando en un div las entradas -->
-<form v-on:submit.prevent="items.push({ id: items.length + 1, label: newItem })" class="add-item fomr">
-    
-    <!-- entrada de texto -->
-    <input
-      v-model.trim="newItem"
-      type="text"
-      placeholder="Add Item">
-    
-    <!-- Caja de seleccion de prioridad -->
-    <label>
-      <input type="checkbox" v-model="newItemHighPriority" />
-      Alta prioridad
-    </label>
-
-    <!-- Boton -->
-    <button class="btn btn-primary"> Guardar </button>
-  
-</form>
-    
-    <ul>
-        <li v-for="({id,label}, i) in items" :key="id"> {{ i+1 }} {{i%2==0?'🔥':'🛍️'}} {{label}} </li>
-    </ul>
+  <div class="content">
+    <fancy-button>
+      <template #icon="{ hover }">
+        {{ hover ? '👻':'👾' }}</template>
+        Hasme clic!
+    </fancy-button>
+  </div>
 </template>
 
 <style scoped>
-.shopping-cart-icon{
-    font-size: 2rem;
-}
 </style>
---
+++ src/components/FancyButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button 
+      @mouseenter="hover = true"
+      @mouseleave="hover = false">
+      <slot name="icon" :hover="hover">⭐</slot>
+      <slot></slot> 
+  </button>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  const hover = ref(false);
+  </script>
+  
+  <style scoped>
+  .content{
+  text-align: center;
+}
+button{
+  width: auto;
+  margin: 0px auto;
+  font-size: 2rem;
+}
+  </style>
